cba9477e4cac8800bef47b1fb8ae1d9e4d2cff77
Sanggita Erinne W.S <sanggita82@gmail.com>
2022-07-22 13:33:05 +0700

    finishing

--- menu/views.py
+++ menu/views.py
@@ -5,5 +5,17 @@ def index(request):
     context = {
         'menu': '/menu',
         'home': '/',
+        'foto1': 'img/product1.jpeg',
+        'foto2': 'img/product2.jpeg',
+        'foto3': 'img/product3.jpeg',
+        'foto4': 'img/product4.png',
+        'foto5': 'img/product5.png',
+        'foto6': 'img/product6.png',
+        'foto7': 'img/product7.png',
+        'foto8': 'img/product8.png',
+        'foto9': 'img/product9.png',
+        'foto10': 'img/product10.png',
+        'foto11': 'img/product11.png',
+        'foto12': 'img/product12.png',
     }
     return render(request, 'menu.html', context)
